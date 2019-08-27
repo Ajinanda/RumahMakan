0992c28c2e3e81e8e7237634292e162eea9020f9
Ajinanda <42225170+Ajinanda@users.noreply.github.com>
2019-08-27 20:01:20 +0800

    First Commit
    Creating Folder And Adding CI Framework

---
+++ WebRumahMakan/application/views/home_screen.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+</head>
+<body>
+	
+
+
+	<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-3.4.1.min.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+</body>
+</html>
